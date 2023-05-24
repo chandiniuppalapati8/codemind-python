13b2de7a22177aea93e55c9ffb669a4470143c37
chandiniuppalapati8 <119730600+chandiniuppalapati8@users.noreply.github.com>
2023-05-24 15:19:49 +0530

    import math for _ in range(int(input())):     n,x,y,k=[int(x) for x in input().split()]     cx=int(n/x)     cy=int(n/y)     clcm=int(n/(x*y)//math.gcd(x,y))     if cx+cy-(2*clcm)>=k:         print("Win")     else:         print("Lose")

---
+++ Appy_and_Contest.py
@@ -0,0 +1,10 @@
+import math
+for _ in range(int(input())):
+    n,x,y,k=[int(x) for x in input().split()]
+    cx=int(n/x)
+    cy=int(n/y)
+    clcm=int(n/(x*y)//math.gcd(x,y))
+    if cx+cy-(2*clcm)>=k:
+        print("Win")
+    else:
+        print("Lose")
