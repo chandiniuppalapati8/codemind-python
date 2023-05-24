c33631c827f3fd10ac3f43152f0fc2f70735a6ae
chandiniuppalapati8 <119730600+chandiniuppalapati8@users.noreply.github.com>
2023-05-24 15:13:26 +0530

    x,y=map(int,input().split()) if x!=0:     if x%2==0:         print("YES")     else:         print("NO") else:     if y%2==0:         print("YES")     else:         print("NO")

---
+++ Can_share_equally?.py
@@ -0,0 +1,11 @@
+x,y=map(int,input().split())
+if x!=0:
+    if x%2==0:
+        print("YES")
+    else:
+        print("NO")
+else:
+    if y%2==0:
+        print("YES")
+    else:
+        print("NO")
